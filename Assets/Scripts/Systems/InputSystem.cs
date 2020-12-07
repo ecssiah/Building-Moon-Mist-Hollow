70492f749fcca35f29bb30915d9b26e0d40b2499
ecssiah <ecssiah@gmail.com>
2020-12-07 14:37:59 -0800

    introduce InputSystem

---
+++ Assets/Scripts/Systems/InputSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputSystem : MonoBehaviour
+{
+    private SelectionHandler selectionHandler;
+
+
+    void Awake()
+    {
+    }
+
+
+    void Start()
+    {
+
+        selectionHandler = gameObject.AddComponent<SelectionHandler>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionHandler.Select();
+        }
+    }
+}
