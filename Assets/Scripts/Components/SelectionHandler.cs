81bef8efae19c62f50f45e767a62639880814c3e
ecssiah <ecssiah@gmail.com>
2020-12-11 01:00:10 -0800

    restricts selection to only the Targets

--- Assets/Scripts/Components/SelectionHandler.cs
+++ Assets/Scripts/Components/SelectionHandler.cs
@@ -13,7 +13,7 @@ public class SelectionHandler : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-        if (hit.collider != null)
+        if (hit.collider != null && hit.collider.gameObject.CompareTag("Target"))
         {
             GameObject entity = hit.transform.parent.gameObject;
 
