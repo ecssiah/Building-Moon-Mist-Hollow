a113f139c53c0b6d372a5b69af084989dd655d03
ecssiah <ecssiah@gmail.com>
2020-12-13 12:51:32 -0800

    fix Utilities functions

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -14,27 +14,18 @@ public static class Utilities
     }
 
 
-    public static Vector3Int WorldToIsoGrid(Vector3 screen)
+    public static Vector3Int WorldToIsoGrid(Vector3 screenPosition)
     {
-        Vector3 IsoVector = WorldToIso(screen);
+        Vector3 isoVector = WorldToIso(screenPosition);
 
         return new Vector3Int(
-            (int)Mathf.Floor(IsoVector.x),
-            (int)Mathf.Floor(IsoVector.y),
+            (int)Mathf.Floor(isoVector.x),
+            (int)Mathf.Floor(isoVector.y),
             0
         );
     }
 
-
-    public static Vector3 IsoToWorld(Vector3 isoVector)
-    {
-        return (1 / 4f) * new Vector3(
-            2 * isoVector.x - 2 * isoVector.y,
-            isoVector.x + isoVector.y, 0
-        );
-    }
-
-
+    
     public static Vector3 WorldToScreen(Vector3 worldPosition)
     {
         return RectTransformUtility.WorldToScreenPoint(
@@ -45,9 +36,21 @@ public static class Utilities
 
     public static Vector3Int ScreenToIsoGrid(Vector3 screenPosition)
     {
-        Vector3 selectedPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-        selectedPosition.y += 0.25f;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        worldPosition.y += 0.25f;
 
-        return Utilities.WorldToIsoGrid(selectedPosition);
+        return Utilities.WorldToIsoGrid(worldPosition);
+    }
+
+
+    public static Vector3 IsoToWorld(Vector3 isoVector)
+    {
+        var matrixProduct = new Vector3(
+            2 * isoVector.x - 2 * isoVector.y,
+            1 * isoVector.x + 1 * isoVector.y,
+            0
+        );
+
+        return (1 / 4f) * matrixProduct;
     }
 }
